cc8d00a5b8526b6218410cec5e8b288d8fd02c12
NestorSo <Nestorsoto2601@gmail.com>
2024-02-06 00:11:31 -0600

    Añadido un nuevo formulario

---
+++ AppOptica/Forms/frmConsulta.xaml.cs
@@ -0,0 +1,24 @@
+namespace AppOptica.Forms;
+
+public partial class frmConsulta : ContentPage
+{
+    public frmConsulta()
+    {
+        InitializeComponent();
+    }
+
+    void OnAgregarClicked(object sender, EventArgs e)
+    {
+        // Código para agregar
+    }
+
+    void OnActualizarClicked(object sender, EventArgs e)
+    {
+        // Código para actualizar
+    }
+
+    void OnEliminarClicked(object sender, EventArgs e)
+    {
+        // Código para eliminar
+    }
+}
